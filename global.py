ab2fd7b261d4f46541dd676b1f5ed441fe09fb2d
Shiying(Rebecca) Cui <queeny.cui@gmail.com>
2017-12-20 20:32:19 +0800

    Create global.py

---
+++ global.py
@@ -0,0 +1,17 @@
+import protocspy.common_pb2 as common_pb2
+import protocspy.common_pb2_grpc as common_pb2_grpc
+import protocspy.broker_pb2 as broker_pb2
+import protocspy.broker_pb2_grpc as broker_pb2_grpc
+import protocspy.market_data_pb2 as market_data_pb2
+import protocspy.market_data_pb2_grpc as market_data_pb2_grpc
+import grpc
+import Order
+import MarketPosition
+
+
+
+
+marketPosition = MarketPosition.MarketPosition()
+order = Order.Order()
+
+
